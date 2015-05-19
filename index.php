6c0ffdd3915854dbd0fc96968fca6f738bccf0fa
Matt Graham <kickthebox@gmail.com>
2015-05-19 12:17:39 -0400

    ios styles

--- index.php
+++ index.php
@@ -6,27 +6,26 @@
 		exec('gpio write 7 1');
 	}
 ?>
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
-		<title>Garage Opener</title>
+		<title>Doorman Beta</title>
 		<link rel="apple-touch-icon" href="apple-touch-icon-iphone.png" />
 		<link rel="apple-touch-icon" sizes="72x72" href="apple-touch-icon-ipad.png" />
 		<link rel="apple-touch-icon" sizes="114x114" href="apple-touch-icon-iphone-retina-display.png" />
 		<link rel="stylesheet" href="/assets/stylesheets/screen.css" type="text/css">
-		<meta name="apple-mobile-web-app-capable" content="yes">
 		<script type="text/javascript" src="/assets/js/jquery-1.10.2.min.js"></script>
 		<script type="text/javascript" src="/assets/js/script.js"></script>
 
+		<meta name="viewport" content="width=device-width">
 	</head>
 	<body>
 		<header class="wrapper">
 			<h1>Doorman<span class="beta">Beta</span></h1>
 		</header>
 		<div class="wrapper main">
-			<div class="button-outside">
-      	<button href='/?trigger=1' class="button">Open Door</button>
-			</div>
+    	<button href='/?trigger=1' class="button closed"></button>
 		</div>
 	</body>
 </html>
